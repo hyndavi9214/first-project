49669a4a69f0168ce0494f27017a9c477ed59ec3
Hyndavi <hyndavihyndavi675@gmail.com>
2023-11-08 17:12:48 +0530

    first commit

---
+++ hyndavi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HyndaviConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hyndavi'
---
+++ hyndavi/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+def silk(request):
+    return HttpResponse('we are not talking about dairy milk')
